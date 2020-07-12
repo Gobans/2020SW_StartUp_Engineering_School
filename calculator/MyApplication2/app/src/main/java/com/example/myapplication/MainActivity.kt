78a1af3cd245dce7feb63fb9317defa53704c26f
Gobans <audghks4452@gmail.com>
2020-07-12 22:52:13 +0900

    2020.07.12

---
+++ calculator/MyApplication2/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.example.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        /**
+         * 레이아웃
+         * (5,4) 테이블 레이아웃
+         */
+
+    }
+}
